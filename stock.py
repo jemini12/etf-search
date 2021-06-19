425fa721351a774304c683cec47d62623232d925
jemin <jemini12@naver.com>
2021-06-19 12:14:58 +0900

    stock crawler error fixed(url error)

--- stock.py
+++ stock.py
@@ -10,7 +10,7 @@ from bs4 import BeautifulSoup
 def get_stock_data(url,sosok):
     documents = []
     for i in range(0,100):
-        res = requests.get(f"{url}?menu=market_sum&returnUrl=http%3A%2F%2Ffinance.naver.com%2Fsise%2Fsise_market_sum.nhn%3F%26sosok={sosok}&page%3D{i}&fieldIds=quant&fieldIds=market_sum&fieldIds=per&fieldIds=roe&fieldIds=frgn_rate&fieldIds=pbr")
+        res = requests.get(f"{url}?menu=market_sum&returnUrl=http%3A%2F%2Ffinance.naver.com%2Fsise%2Fsise_market_sum.nhn%3F%26sosok%3D{sosok}%26page%3D{i}&fieldIds=quant&fieldIds=market_sum&fieldIds=per&fieldIds=roe&fieldIds=frgn_rate&fieldIds=pbr")
         data = BeautifulSoup(res.text,"lxml")
         tableHead = data.select("#contentarea > div.box_type_l > table.type_2 > thead")
         tableBody = data.select("#contentarea > div.box_type_l > table.type_2 > tbody > tr")
@@ -57,13 +57,14 @@ def main():
     logger = logging.getLogger(__name__)
     logging.info("stock crawling start")
     #https://finance.naver.com/sise/sise_market_sum.nhn?sosok=0
+    #https://finance.naver.com/sise/field_submit.nhn?menu=market_sum&returnUrl=http%3A%2F%2Ffinance.naver.com%2Fsise%2Fsise_market_sum.nhn%3Fsosok%3D1%26page%3D3&fieldIds=quant&fieldIds=market_sum&fieldIds=per&fieldIds=roe&fieldIds=listed_stock_cnt&fieldIds=pbr
     es = Elasticsearch(
     hosts=[{'host': "localhost", 'port': "9200"}])
     try: 
         documents = get_stock_data("https://finance.naver.com/sise/field_submit.nhn",0)
         helpers.bulk(es, documents)
         documents = get_stock_data("https://finance.naver.com/sise/field_submit.nhn",1)
-        # helpers.bulk(es, documents)
+        helpers.bulk(es, documents)
         logging.info("stock crawling end")
     except Exception as e:
         logging.info(e)
