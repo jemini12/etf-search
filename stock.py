310696ad1a902103bcdd2ef28133e7a9f29ae72c
jemin <jemini12@naver.com>
2021-06-19 12:21:14 +0900

    fix base 10(,) error for price

--- stock.py
+++ stock.py
@@ -25,7 +25,7 @@ def get_stock_data(url,sosok):
                 stockData = {
                     "stockName" : tableCell[1].getText() if tableCell[1].getText() != "N/A" else None,
                     "code": code,
-                    "price": int(tableCell[2].getText()) if tableCell[2].getText() != "N/A"  else None,
+                    "price": int(tableCell[2].getText().replace(",","")) if tableCell[2].getText() != "N/A"  else None,
                     "per":  float(tableCell[9].getText()) if tableCell[9].getText() != "N/A"  else None,
                     "roe":  float(tableCell[10].getText()) if tableCell[10].getText() != "N/A" else None,
                     "pbr":  float(tableCell[11].getText()) if tableCell[11].getText() != "N/A" else None,
